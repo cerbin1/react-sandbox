25661f8b085f6d0951e291a266ec8a0ab959ed3a
bartek <gekarez@gmail.com>
2020-03-04 22:14:38 +0100

    Update first assignment content

--- src/assignment_1/Main.js
+++ src/assignment_1/Main.js
@@ -28,12 +28,27 @@ class Main extends Component {
         };
 
         return (
-            <div className="Main">
-                <UserOutput username={this.state.username}/>
-                <UserOutput username={this.state.username}/>
-                <UserOutput username={this.state.username}/>
-                <button style={buttonStyle} onClick={this.changeUsername}>Change username</button>
-                <UserInput changed={this.changeUsernameHandler} username={this.state.username}/>
+            <div>
+                <div className="Main">
+                    <UserOutput username={this.state.username}/>
+                    <UserOutput username={this.state.username}/>
+                    <UserOutput username={this.state.username}/>
+                    <button style={buttonStyle} onClick={this.changeUsername}>Change username</button>
+                    <UserInput changed={this.changeUsernameHandler} username={this.state.username}/>
+
+                </div>
+                <div><ol>
+                    <li>Create TWO new components: UserInput and UserOutput</li>
+                    <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+                    <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+                    <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+                    <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+                    <li>Add a method to manipulate the state (=> an event-handler method)</li>
+                    <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+                    <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+                    <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+                    <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+                </ol></div>
             </div>
         );
     }
