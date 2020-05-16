30bbaacbac4f315af60920deb9491dd578e3c664
cerbin1 <gekarez@gmail.com>
2020-05-16 12:28:34 +0200

    Add lists and conditions resources

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Main from './assignment_1/Main'
 import './index.css';
 
 ReactDOM.render(
-  <Main />,
+  <App />,
   document.getElementById('root')
 );
