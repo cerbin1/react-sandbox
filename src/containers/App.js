c974ee188eca1d6e4ac1a7a68b5871f871acddf8
cerbin1 <gekarez@gmail.com>
2020-07-10 13:36:29 +0200

    Improve project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import Main2 from "./assignment_2/Main2";
-import Person from "./Person/Person";
-import Persons from "./Person/Persons";
+import Persons from "../components/Persons/Persons";
 
 const App = () => {
 
