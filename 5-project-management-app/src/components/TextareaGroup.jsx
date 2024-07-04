ba9f577f40f9fae6efccc8f87a2a0195a4eb2f5d
cerbin1 <gekarez@gmail.com>
2024-07-04 16:52:08 +0200

    Add outline focus to textarea component

--- 5-project-management-app/src/components/TextareaGroup.jsx
+++ 5-project-management-app/src/components/TextareaGroup.jsx
@@ -6,7 +6,7 @@ const TextareaGroup = forwardRef(function TextareaGroup({ label }, ref) {
       <label className="uppercase text-gray-600 font-bold">
         {label}
         <textarea
-          className="w-full py-2 px-4 border-b-2 border-neutral-400 bg-neutral-300"
+          className="w-full py-2 px-4 border-b-2 border-neutral-400 bg-neutral-300 focus:outline-none focus:border-stone-600"
           rows={3}
           ref={ref}
         />
