916c587c25ccc6e269021157fe0dc636a519fab7
cerbin1 <gekarez@gmail.com>
2024-07-04 19:17:09 +0200

    Add validation and display modal on invalid data input

--- 5-project-management-app/src/components/CreateProjectForm.jsx
+++ 5-project-management-app/src/components/CreateProjectForm.jsx
@@ -1,13 +1,31 @@
 import { useRef } from "react";
 import InputGroup from "./InputGroup";
 import TextareaGroup from "./TextareaGroup";
+import Modal from "./Modal";
 
-export default function CreateProjectForm({ createProject, cancelCreatingProject }) {
+export default function CreateProjectForm({
+  createProject,
+  cancelCreatingProject,
+}) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
 
   function handleProjectSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
     createProject({
       title: title.current.value,
       description: description.current.value,
@@ -17,6 +35,9 @@ export default function CreateProjectForm({ createProject, cancelCreatingProject
 
   return (
     <div className="w-4/5 text-center ">
+      <Modal ref={modal}>
+        <h3 className="text-xl font-bold text-zinc-700 py-4">Invalid input</h3>
+      </Modal>
       <div className="flex flex-col mt-36 w-1/2 mx-auto ">
         <div className="flex justify-end">
           <button
