6b7d1a1f72c6364e57bcbcdfac9d0773ac2b1b09
cerbin1 <gekarez@gmail.com>
2024-07-04 16:51:30 +0200

    Add outline focus to input elements

--- 5-project-management-app/src/components/InputGroup.jsx
+++ 5-project-management-app/src/components/InputGroup.jsx
@@ -7,7 +7,7 @@ const InputGroup = forwardRef(function InputGroup({ label, type }, ref) {
         {label}
         <input
           type={type}
-          className="w-full py-2 px-4 border-b-2 border-neutral-400 bg-neutral-300"
+          className="w-full py-2 px-4 border-b-2 border-neutral-400 bg-neutral-300 focus:outline-none focus:border-stone-600"
           ref={ref}
         />
       </label>
