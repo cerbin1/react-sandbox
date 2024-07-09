1adb33dcc370c6cf33940c181a555ff6c298da7d
cerbin1 <gekarez@gmail.com>
2024-07-09 11:11:56 +0200

    Add questions and answers - skeleton

--- 8-project-quiz-app/src/App.jsx
+++ 8-project-quiz-app/src/App.jsx
@@ -1,7 +1,33 @@
 import Header from "./components/Header";
+import questions from "./questions";
+import { useState } from "react";
 
 function App() {
-  return <Header />;
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  const question = questions[questionIndex];
+
+  return (
+    <>
+      <Header />
+      <div id="quiz">
+        <div id="question-overview">
+          <div id="question">
+            <h2>{question.text}</h2>
+          </div>
+          <div id="answers">
+            {question.answers.map((answer) => (
+              <div key={answer} className="answer">
+                <button onClick={() => setQuestionIndex(questionIndex + 1)}>
+                  {answer}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
