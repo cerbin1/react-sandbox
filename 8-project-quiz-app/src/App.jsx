c265c9126f98a161952d704e0a1f47a7ce6d3c95
cerbin1 <gekarez@gmail.com>
2024-07-09 11:15:20 +0200

    Add game over when all questions are answered

--- 8-project-quiz-app/src/App.jsx
+++ 8-project-quiz-app/src/App.jsx
@@ -4,28 +4,37 @@ import { useState } from "react";
 
 function App() {
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
   const question = questions[questionIndex];
+
+  function handleQuestionAnswer() {
+    setQuestionIndex(questionIndex + 1);
+    if (questionIndex === questions.length - 1) {
+      setGameOver(true);
+    }
+  }
 
   return (
     <>
       <Header />
-      <div id="quiz">
-        <div id="question-overview">
-          <div id="question">
-            <h2>{question.text}</h2>
-          </div>
-          <div id="answers">
-            {question.answers.map((answer) => (
-              <div key={answer} className="answer">
-                <button onClick={() => setQuestionIndex(questionIndex + 1)}>
-                  {answer}
-                </button>
-              </div>
-            ))}
+      {!gameOver && (
+        <div id="quiz">
+          <div id="question-overview">
+            <div id="question">
+              <h2>{question.text}</h2>
+            </div>
+            <div id="answers">
+              {question.answers.map((answer) => (
+                <div key={answer} className="answer">
+                  <button onClick={handleQuestionAnswer}>{answer}</button>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {gameOver && <p>Game Over</p>}
     </>
   );
 }
