e21e890865350b0938ee9e7788dfae4de96c8415
cerbin1 <gekarez@gmail.com>
2024-07-11 16:15:49 +0200

    Add example validation on form submission

--- 17-forms/src/components/Login.jsx
+++ 17-forms/src/components/Login.jsx
@@ -1,12 +1,23 @@
 import { useRef } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(email.current.value + ", " + password.current.value);
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   return (
@@ -17,6 +28,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && "Please enter a valid email address."}
+          </div>
         </div>
 
         <div className="control no-margin">
