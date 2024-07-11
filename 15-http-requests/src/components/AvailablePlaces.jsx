f2792c6305da4aa5d8bfaa31e7a4fe9f09ef3e0e
cerbin1 <gekarez@gmail.com>
2024-07-11 10:35:54 +0200

    Change synchronous fetch to async await

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -6,9 +6,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => response.json())
-      .then((data) => setAvailablePlaces(data.places));
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+    }
+    fetchPlaces();
   }, []);
 
   return (
