cdedc8ee8d3fa45c341527284afdc88d3d8c34d8
cerbin1 <gekarez@gmail.com>
2024-07-16 22:22:45 +0200

    Add sending requests to backend on redux state change

--- 20-advanced-redux/src/App.js
+++ 20-advanced-redux/src/App.js
@@ -1,11 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-http-14d07-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
