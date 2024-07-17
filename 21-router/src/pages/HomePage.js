ebf7d436a31d70fe4fa859dc7469a12751923600
cerbin1 <gekarez@gmail.com>
2024-07-17 19:10:43 +0200

    Add root layout and header

--- 21-router/src/pages/HomePage.js
+++ 21-router/src/pages/HomePage.js
@@ -1,11 +1,7 @@
-import { Link } from "react-router-dom";
-
 export default function HomePage() {
   return (
     <>
       <h1>My Home Page</h1>
-
-      <Link to="/products">Products</Link>
     </>
   );
 }
--- 21-router/src/pages/Products.js
+++ 21-router/src/pages/Products.js
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-
 export default function ProductsPage() {
   return (
     <>
       <h1>The Products Page</h1>
-      <Link to="/">Home</Link>
     </>
   );
 }
