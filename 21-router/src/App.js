44f81c556a9c066bfd09975fb187b4d7fed9ef29
cerbin1 <gekarez@gmail.com>
2024-07-17 18:54:44 +0200

    Add second route Products

--- 21-router/src/App.js
+++ 21-router/src/App.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
   },
 ]);
 
