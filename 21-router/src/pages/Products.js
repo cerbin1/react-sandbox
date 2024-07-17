6a156943ac3780ad25fe99e2e1846806b37f4828
cerbin1 <gekarez@gmail.com>
2024-07-17 19:00:52 +0200

    Add navigation links:

--- 21-router/src/pages/Products.js
+++ 21-router/src/pages/Products.js
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function ProductsPage() {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <Link to="/">Home</Link>
+    </>
+  );
 }
