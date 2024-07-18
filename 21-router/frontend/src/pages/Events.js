4e3b300808c22e687c9537f9b9dad27e27357bec
cerbin1 <gekarez@gmail.com>
2024-07-18 22:53:31 +0200

    Fix backend url link

--- 21-router/frontend/src/pages/Events.js
+++ 21-router/frontend/src/pages/Events.js
@@ -16,7 +16,7 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventss");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     json({ message: "Could not fetch events" }, { status: 500 });
