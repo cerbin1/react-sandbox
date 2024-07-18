74d06f7648e0e024be4b4980eb50dc451f9e978d
cerbin1 <gekarez@gmail.com>
2024-07-18 15:54:55 +0200

    Add Back button in ProductDetails component

--- 21-router/src/pages/ProductDetails.js
+++ 21-router/src/pages/ProductDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function ProductDetailsPage() {
   const params = useParams();
@@ -8,6 +8,9 @@ export default function ProductDetailsPage() {
       <h1>Product Details</h1>
 
       <p>{params.id}</p>
+      <Link to=".." relative="path">
+        Back
+      </Link>
     </>
   );
 }
