078b6e190e931fe59eb86fead6a8cb8e849c2fc0
cerbin1 <gekarez@gmail.com>
2024-07-19 10:50:18 +0200

    Implement deleting event with using programatically sent action

--- 21-router/frontend/src/App.js
+++ 21-router/frontend/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailsPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from "./pages/EventDetails";
 import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -32,7 +33,11 @@ const router = createBrowserRouter([
             id: "event-details",
             loader: eventDetailLoader,
             children: [
-              { index: true, element: <EventDetailsPage /> },
+              {
+                index: true,
+                element: <EventDetailsPage />,
+                action: deleteEventAction,
+              },
               { path: "edit", element: <EditEventPage /> },
             ],
           },
--- 21-router/frontend/src/pages/EventDetails.js
+++ 21-router/frontend/src/pages/EventDetails.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
@@ -20,5 +20,17 @@ export async function loader({ params }) {
     );
   } else {
     return response;
+  }
+}
+
+export async function action({ params, request }) {
+  const response = await fetch(
+    "http://localhost:8080/events/" + params.eventId,
+    { method: request.method }
+  );
+  if (!response.ok) {
+    json({ message: "Could not delete event." }, { status: 500 });
+  } else {
+    return redirect("/events");
   }
 }
