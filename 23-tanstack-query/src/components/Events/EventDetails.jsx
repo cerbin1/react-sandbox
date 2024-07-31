2cc71ad027f9e9f5232e90ead4d5e4889364f6dc
cerbin1 <gekarez@gmail.com>
2024-07-31 15:24:48 +0200

    wip

--- 23-tanstack-query/src/components/Events/EventDetails.jsx
+++ 23-tanstack-query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,43 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvent } from "../../util/http.js";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["event-details"],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+  console.log(data);
+
+  let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+  if (isError) {
+    content = <p>Error ocurred while fetchin event details...</p>;
+  }
+  if (data) {
+    content = (
+      <div id="event-details-content">
+        <img
+          src={`http://localhost:3000/${data.image}`}
+          alt="Event details image"
+          data
+        />
+        <div id="event-details-info">
+          <div>
+            <p id="event-details-location">{data.location}</p>
+            <time dateTime={`Todo-DateT$Todo-Time`}>{data.date}</time>
+          </div>
+          <p id="event-details-description">{data.description}</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -13,22 +48,13 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data.title}</h1>
           <nav>
             <button>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
+        {content}
       </article>
     </>
   );
--- 23-tanstack-query/src/util/http.js
+++ 23-tanstack-query/src/util/http.js
@@ -55,3 +55,19 @@ export async function fetchSelectableImages({ signal }) {
   const { images } = await response.json();
   return images;
 }
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal: signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+  return event;
+}
