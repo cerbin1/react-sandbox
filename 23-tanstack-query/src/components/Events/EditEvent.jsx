8ce12c78b78d532c7868b2e6fb831e4aa23e7a4f
cerbin1 <gekarez@gmail.com>
2024-08-01 11:08:57 +0200

    Implement optimistc updating to update event

--- 23-tanstack-query/src/components/Events/EditEvent.jsx
+++ 23-tanstack-query/src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { fetchEvent, updateEvent } from "../../util/http.js";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -18,6 +18,23 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", id] });
+
+      const previousEvent = queryClient.getQueryData(["events", id]);
+
+      queryClient.setQueryData(["events", id], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["events", id] });
+    },
   });
 
   function handleSubmit(formData) {
