30c18957d67c61f656e267eb3130a543f1f56b05
cerbin1 <gekarez@gmail.com>
2024-08-15 19:10:13 +0200

    Extract SubmitForm component

--- 18-project-food-order-app/src/App.jsx
+++ 18-project-food-order-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Meals from "./components/Meals";
 import Modal from "./components/Modal";
 import Cart from "./components/Cart";
+import SubmitForm from "./components/SubmitForm";
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -121,42 +122,13 @@ function App() {
         />
       </Modal>
       <Modal open={checkoutModalOpen}>
-        <h2>Checkout</h2>
-        <p>Total Amount: ${cartTotal}</p>
-        <form onSubmit={handleSubmit}>
-          <div className="control">
-            <label htmlFor="name">Full Name</label>
-            <input name="name" type="text" id="name" required />
-          </div>
-          <div className="control">
-            <label htmlFor="email">E-Mail Address</label>
-            <input name="email" type="email" id="email" required />
-          </div>
-          <div className="control">
-            <label htmlFor="street">Street</label>
-            <input name="street" type="text" id="street" required />
-          </div>
-          <div className="control-row">
-            <div className="control">
-              <label htmlFor="postal-code">Postal Code</label>
-              <input name="postal-code" type="text" id="postal-code" required />
-            </div>
-            <div className="control">
-              <label htmlFor="city">City</label>
-              <input name="city" type="text" id="city" required />
-            </div>
-          </div>
-          <div className="modal-actions">
-            <span className="text-button" onClick={closeCheckoutModal}>
-              Close
-            </span>
-            <button disabled={submitting} className="button">
-              {submitting ? "Submitting..." : "Submit Order"}
-            </button>
-          </div>
-        </form>
-
-        {submitError && <p>{submitError}</p>}
+        <SubmitForm
+          total={cartTotal}
+          onModalClose={closeCheckoutModal}
+          onFormSubmit={handleSubmit}
+          submitting={submitting}
+          submitError={submitError}
+        />
       </Modal>
     </>
   );
---
+++ 18-project-food-order-app/src/components/SubmitForm.jsx
@@ -0,0 +1,48 @@
+export default function SubmitForm({
+  total,
+  onFormSubmit,
+  onModalClose,
+  submitting,
+  submitError,
+}) {
+  return (
+    <>
+      <h2>Checkout</h2>
+      <p>Total Amount: ${total}</p>
+      <form onSubmit={onFormSubmit}>
+        <div className="control">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" type="text" id="name" required />
+        </div>
+        <div className="control">
+          <label htmlFor="email">E-Mail Address</label>
+          <input name="email" type="email" id="email" required />
+        </div>
+        <div className="control">
+          <label htmlFor="street">Street</label>
+          <input name="street" type="text" id="street" required />
+        </div>
+        <div className="control-row">
+          <div className="control">
+            <label htmlFor="postal-code">Postal Code</label>
+            <input name="postal-code" type="text" id="postal-code" required />
+          </div>
+          <div className="control">
+            <label htmlFor="city">City</label>
+            <input name="city" type="text" id="city" required />
+          </div>
+        </div>
+        <div className="modal-actions">
+          <span className="text-button" onClick={onModalClose}>
+            Close
+          </span>
+          <button disabled={submitting} className="button">
+            {submitting ? "Submitting..." : "Submit Order"}
+          </button>
+        </div>
+      </form>
+
+      {submitError && <p>{submitError}</p>}
+    </>
+  );
+}
