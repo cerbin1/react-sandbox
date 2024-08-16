32f08d93ad3c67be77217aca7e705f7a5c0198ad
cerbin1 <gekarez@gmail.com>
2024-08-16 13:47:42 +0200

    Add Accordion and AccordionItem components to introduce Compound Components

--- 27-patterns-and-best-practices/src/App.jsx
+++ 27-patterns-and-best-practices/src/App.jsx
@@ -1,5 +1,31 @@
+import Accordion from "./components/accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <Accordion.Item
+            id="experience"
+            className="accordion-item"
+            title="We got 20 yers of experience"
+          >
+            <p>You cannot go wrong with us.</p>
+            <p>Lorem ipsum</p>
+          </Accordion.Item>
+          <Accordion.Item
+            id="local-guides"
+            className="accordion-item"
+            title="We are working with local guides"
+          >
+            <p>We are not doing this along from our office.</p>
+            <p>Lorem ipsum</p>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
